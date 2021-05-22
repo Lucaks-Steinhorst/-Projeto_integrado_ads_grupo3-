5d99ecb7112e242a8569737c9fa378b75e98fd71
deoliveira-angela <82618941+deoliveira-angela@users.noreply.github.com>
2021-05-22 16:20:53 -0300

    Add files via upload

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,27 +3,27 @@ import {promisify} from "util";
 import authConfig from "../config/auth.js";
 
 export default async(req, res, next) => {
-    /**
-     * Implementar o middleware auth.js aqui
-     */
-    const authHeader = req.headers.authorization;
+    
+    const authHeader = req.headers.authorization
     if(!authHeader) {
         return res.status(401).json({
             error: true,
-            message: "Erro: token não encontrado!"
+            message: "Token não encontrado"
         })
     }
 
-    //Bearer <Token>
-    const [, token] = authHeader.split(" ");
+    //Bearer <token>
+    const [, token] = authHeader.split(" ")
+
     try {
-        const decode = await promisify(jwt.verify)(token, authConfig.secret);
-        req.userID = decode.id;
-        return next();
-    } catch(err) {
+        const decode = await promisify(jwt.verify)(token, authConfig.secret)
+        req.userID = decode.id
+        return next()
+    } catch (error) {
         return res.status(401).json({
             error: true,
-            message: "Erro: token inválido!"
-        });
+            message: "Token inválido"
+        })
     }
 }
+
